c656a8bf023d6caa5de46b5ec8165869ccdbdabc
francis <go717franciswang@gmail.com>
2014-02-20 18:53:13 -0800

    Clean up

--- src/cljs/explore_c2/core.cljs
+++ src/cljs/explore_c2/core.cljs
@@ -4,18 +4,6 @@
             [c2.svg :as svg]
             [c2.core :as c2]))
 
-(def root
-  {:node "S"
-   :children [{:node "S"
-               :children [{:node "("} 
-                          {:node "S" 
-                           :children [{:node "("} 
-                                      {:node ")"}]}
-                          {:node ")"}]}
-              {:node "S"
-               :children [{:node "("}
-                          {:node ")"}]}]})
-
 (defn get-center [nodes]
   (let [a (first nodes)
         b (last nodes)]
@@ -90,62 +78,32 @@
         (:children node))
       legends)))
 
-(let [tree (bind-location root)
-      [max-x max-y] (get-max tree)
-      width (* (+ 2 max-x) unit-width)
-      height (* (+ 2 max-y) unit-height)
-      lines (get-lines tree (:children tree))
-      legends (get-legends tree)
-      svg-ele [:svg#tree {:height height :width width}]]
-  (pp legends)
-  (bind! "#tree" (vec (concat svg-ele lines legends))))
+(defn draw-tree [root container-selector]
+  (let [tree (bind-location root)
+        [max-x max-y] (get-max tree)
+        width (* (+ 2 max-x) unit-width)
+        height (* (+ 2 max-y) unit-height)
+        lines (if (contains? tree :children)
+                (get-lines tree (:children tree))
+                [])
+        legends (get-legends tree)
+        container-nodename (.-nodeName (c2.dom/select container-selector))
+        svg-ele [:svg {:height height :width width}]]
+    (bind! container-selector [container-nodename (vec (concat svg-ele lines legends))])))
 
-(repl/connect "http://betalabs:9000/repl")
+(defn ^:export demo []
+  (let [root {:node "S"
+              :children [{:node "S"
+                          :children [{:node "("} 
+                                     {:node "S" 
+                                      :children [{:node "("} 
+                                                 {:node ")"}]}
+                                     {:node ")"}]}
+                         {:node "S"
+                          :children [{:node "("}
+                                     {:node ")"}]}]}]
+    (draw-tree root "#tree")))
+
+;(repl/connect "http://betalabs:9000/repl" )
 ;(repl/connect "http://localhost:9000/repl")
-
-; (def coordinate 
-;   {:x 100 :y 200})
-; 
-; (pp coordinate)
-; (pp (svg/->xy coordinate))
-; (pp (svg/->xy [100 200]))
-; 
-; (pp {:a 150 :b [:c :d]})
-; 
-; (pp (svg/scale coordinate))
-; (pp (svg/translate coordinate))
-; 
-; (pp (svg/rotate coordinate))
-; 
-; (pp (svg/get-bounds (.getElementById js/document "s1")))
-; 
-; (pp (svg/circle [50 50] 10))
-; 
-; (pp (svg/arc {:inner-radius 10 :outer-radius 15 :angle-offset 1}))
-
-#_(bind! "#barchart"
-       [:div#barchart
-        [:h2 "Rad barchart!"]
-        [:div.bars
-         (c2/unify {"A" 1, "B" 2, "C" 4, "D" 3}
-                (fn [[label val]]
-                  [:div.bar
-                   [:div.bar-fill {:style {:width val}}]
-                   [:span.label label]]))]])
-
-(bind! "#mydiv" 
-       [:div#mydiv 
-        (c2/unify {"a" 1} 
-                  (fn [[k v]] 
-                    [:div k v]))])
-
-(let [data (atom [0])
-      random-update (fn []
-                      (let [r (rand)]
-                        ;(p (str r))
-                        (reset! data [r])))
-                        ;(swap! data assoc 0 r)))
-      ]
-  (bind! "#mydiv" [:div#mydiv (c2/unify @data (fn [v] [:div v]))])
-  (interval 1000 (random-update)))
 
